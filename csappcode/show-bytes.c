ee782fbfdb2533322b794141e4e4e6077ba31d58
jackpan <826647196@qq.com>
2022-01-25 14:19:09 +0800

    Create show-bytes.c

---
+++ csappcode/show-bytes.c
@@ -0,0 +1,53 @@
+//
+//  show-bytes.c
+//  csappcode
+//
+//  Created by JackPan on 2022/1/23.
+//
+
+#include <stdio.h>
+#include <string.h>
+typedef unsigned char *byte_pointer;
+
+void show_bytes(byte_pointer start, size_t len) {
+    size_t i;
+    for (i = 0; i < len; i++) {
+        printf(" %.2x", start[i]);
+    }
+    printf("\n");
+}
+
+void show_int(int x) {
+    show_bytes((byte_pointer) &x, sizeof(int));
+}
+
+void show_float(float x) {
+    show_bytes((byte_pointer) &x, sizeof(float));
+}
+
+void show_pointer(void *x) {
+    show_bytes((byte_pointer) &x, sizeof(void *));
+}
+
+void test_show_bytes(int val) {
+    int ival = val;
+    float fval = (float) ival;
+    int *pval = &ival;
+    show_int(ival);
+    show_float(fval);
+    show_pointer(pval);
+}
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    // example 1
+//    test_show_bytes(12345);
+    
+    // example 2
+    const char *cha = "12345";
+    int i = 6;
+    show_bytes((byte_pointer) cha, sizeof(cha));
+    show_bytes((byte_pointer) &i, sizeof(i));
+    
+    return 0;
+}
